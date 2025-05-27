bc41804e278d10ef1178bd3e0809b0ba4595f88a
Dipak Singh <dipaksingh3323@gmail.com>
2025-05-27 23:53:11 +0530

    userModel Done.

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    nmae: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartItems: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false, timestamps: true }
+);
+
+const user = mongoose.models.user || mongoose.model("user", userSchema);
+export default user;
